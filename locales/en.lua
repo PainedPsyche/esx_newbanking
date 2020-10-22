cfea9a11f78ab50ae7aee56fa0265b55ae2cb013
PainedPsyche <pained.psyche@protonmail.com>
2020-10-22 20:30:19 +0200

    renamed and moved client/server scripts to closer match the convention used in es_extended, added localization of lua files based on es_extended

---
+++ locales/en.lua
@@ -0,0 +1,13 @@
+Locales['en'] = {
+    ['bank'] = 'Bank',
+    ['bank_pacific'] = 'Pacific Bank',
+    ['atm'] = 'ATM',
+    ['access_account'] = 'Press ~INPUT_PICKUP~ to access your account ~b~',
+    ['success_deposit'] = 'Deposit completed.',
+    ['success_withdraw'] = 'Withdrawal completed.',
+    ['success_transfer'] = 'Transfer completed.',
+    ['error_amount'] = 'Invalid amount.',
+    ['error_recipient'] = 'Recipient not found.',
+    ['error_self'] = 'You cannot transfer funds to your own account.',
+    ['error_insufficient'] = 'Insufficient funds.',
+}
---
+++ locales/fr.lua
@@ -0,0 +1,10 @@
+Locales['fr'] = {
+    ['access_account'] = 'Appuie sur ~INPUT_PICKUP~ pour accèder à tes comptes ~b~',
+    ['success_deposit'] = 'Dépot effectué.',
+    ['success_withdraw'] = 'Retrait effectué.',
+    ['success_transfer'] = 'Transfert effectué.',
+    ['error_amount'] = 'Montant invalide.',
+    ['error_recipient'] = 'Destinataire introuvable.',
+    ['error_self'] = 'Vous ne pouvez pas faire de transfert à vous même.',
+    ['error_insufficient'] = 'Vous n\'avez pas assez d\'argent en banque.',
+}
